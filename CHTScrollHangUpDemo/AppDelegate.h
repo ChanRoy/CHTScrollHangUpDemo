361c9c3b31b3033e4b159907c8d2073b2208d1ab
ChanRoy <chen_ht89@163.com>
2017-02-15 15:59:56 +0800

    initial commit

---
+++ CHTScrollHangUpDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CHTScrollHangUpDemo
+//
+//  Created by cht on 2017/2/15.
+//  Copyright © 2017年 cht. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CHTScrollHangUpDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CHTScrollHangUpDemo
+//
+//  Created by cht on 2017/2/15.
+//  Copyright © 2017年 cht. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
